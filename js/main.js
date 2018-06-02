143aa473a36577b51eccbffbb80fe0e0a0ec6310
Ettore Stark <ettoreastark@gmail.com>
2018-06-02 10:17:54 -0400

    Nothing to say...

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 (function(window, document) {
-	var element = document.getElementById('show');
+	var x = document.getElementById('x');
+	var y = document.getElementById('y');
+	var z = document.getElementById('z');
 	function accelerometer(e) {
-		var x = event.acceleration.x + "m/s";
-	    var y = event.acceleration.y + "m/s";
-	    var z = event.acceleration.z + "m/s";
-	    element.innerHTML = "<b>x: </b>" + x + "<br/><b>y: </b>" + y + "<br/><b>z: </b>" + z;
+		var xC = Math.round(event.accelerationIncludingGravity.x*100)/100;
+	    var yC = Math.round(event.accelerationIncludingGravity.y*100)/100;
+	    var zC = Math.round(event.accelerationIncludingGravity.z*100)/100;
+	    x.innerHTML = 'X: ' + xC;
+	    y.innerHTML = 'Y: ' + yC;
+	    z.innerHTML = 'Z: ' + zC;
 	}
 
 	window.addEventListener('devicemotion', accelerometer);
